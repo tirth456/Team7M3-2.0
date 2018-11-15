e74fed63aa36ad15b0a74e3cafeeff44c1e7a6eb
Tirthraj Chauhan <tirthraj456@gmail.com>
2018-11-14 19:16:36 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,43 @@
 var SERVER_NAME = 'patients-api'
+var DEFAULT_PORT = 7000
+var DEFAULT_HOST = '127.0.0.1'
+
+var http = require ('http');
+var mongoose = require ("mongoose");
+
 var port = process.env.PORT;
-// var DEFAULT_PORT = 7000
-// var DEFAULT_HOST = '127.0.0.1'
-
-//var http = require ('http');
-//var mongoose = require ("mongoose");
-
-///var port = process.env.PORT;
-//var ipaddress = process.env.IP; // TODO: figure out which IP to use for the heroku
+var ipaddress = process.env.IP; // TODO: figure out which IP to use for the heroku
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.  
-// var uristring = 
-//   process.env.MONGODB_URI || 
-//   'mongodb://localhost/patient-db';
+var uristring = 
+  process.env.MONGODB_URI || 
+  'mongodb://localhost/patient-db';
 
-// // Makes connection asynchronously.  Mongoose will queue up database
-// // operations and release them when the connection is complete.
-// mongoose.connect(uristring, function (err, res) {
-//   if (err) { 
-//     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-//   } else {
-//     console.log ('Successfully connected to: ' + uristring);
-//   }
-// });
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Successfully connected to: ' + uristring);
+  }
+});
 
 // This is the schema.  Note the types, validation and trim
 // statements.  They enforce useful constraints on the data.
-// var patientSchema = new mongoose.Schema({
-// 		first_name: String, 
-//     last_name: String, 
-//     dob: String,
-// 		address: String,
-// 		department: String,
-// 		doctor: String
-// });
+var patientSchema = new mongoose.Schema({
+		first_name: String, 
+    last_name: String, 
+    dob: String,
+		address: String,
+		department: String,
+		doctor: String
+});
 
-// schema for patient records
-
-// var recordSchema = new mongoose.Schema({
-//   patient_id: mongoose.Schema.Types.ObjectId,
-//   blood_pressure: String,
-//   resp_rate: String,
-//   blood_oxygen: String,
-//   record_date: String
-// });
-
-// // Compiles the schema into a model, opening (or creating, if
-// // nonexistent) the 'Patients' collection in the MongoDB database
-// var Patients = mongoose.model('Patient', patientSchema);
-
-// var Records = mongoose.model('Record',recordSchema);
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'Patients' collection in the MongoDB database
+var Patients = mongoose.model('Patient', patientSchema);
 
 var restify = require('restify')
 
@@ -60,19 +47,19 @@ var restify = require('restify')
   // creating the restify server
   , server = restify.createServer({ name: SERVER_NAME})
 
-  // if (typeof ipaddress === "undefined") {
-	// 	//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-	// 	//  allows us to run/test the app locally.
-	// 	console.warn('No process.env.IP var, using default: ' + DEFAULT_HOST);
-	// 	ipaddress = DEFAULT_HOST;
-	// };
+  if (typeof ipaddress === "undefined") {
+		//  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+		//  allows us to run/test the app locally.
+		console.warn('No process.env.IP var, using default: ' + DEFAULT_HOST);
+		ipaddress = DEFAULT_HOST;
+	};
 
-	// if (typeof port === "undefined") {
-	// 	console.warn('No process.env.PORT var, using default port: ' + DEFAULT_PORT);
-	// 	port = DEFAULT_PORT;
-	// };
+	if (typeof port === "undefined") {
+		console.warn('No process.env.PORT var, using default port: ' + DEFAULT_PORT);
+		port = DEFAULT_PORT;
+	};
 
-  server.listen(PORT, function () {
+  server.listen(port, ipaddress, function () {
   console.log('Server %s listening at %s', server.name, server.url)
   console.log('Endpoints:')
   console.log(server.url+'/patients', 'To fetch all pateints(GET Method)')
@@ -93,17 +80,17 @@ server.get('/patients', function (req, res, next) {
 
   console.log('GET request: patient');
     // Find every entity within the given collection
-    patientsSave.find({}, function (error, patients) {
-      // return all of the patients in the system
-      response.send(patients)
-    })
+    Patients.find({}).exec(function (error, result) {
+      if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+      res.send(result);
+    });
 })
 
 // Get a single patient by their user id
 server.get('/patients/:id', function (req, res, next) {
 
   // Find a single patient  by their id within save
-  patientsSave.findOne({ _id: req.params.id }, function (error, patient)  {
+  patientsSave.findOne({ _id: req.params.id }, function (error, patient) {
     // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
@@ -148,118 +135,34 @@ server.post('/patients', function (request, res, next) {
     return next(new restify.InvalidArgumentError('Doctor must be supplied'))
   }
 
-  var newPatient =  {
+  var newPatient = new Patients({
     first_name: request.params.first_name, 
     last_name: request.params.last_name,
     dob: request.params.dob,
     address: request.params.address,
     department: request.params.department,
     doctor: request.params.doctor
-  };
+  });
 
  // Create the patient and saving to db
- patientsSave.create( newPatient, function (error, patients) {
+ newPatient.save(function (error, result) {
 
   // If there are any errors, pass them to next in the correct format
   if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
   // Send the patient if no issues
-  response.send(201, patients)
+  res.send(201, result)
 })
 })
 
 // Delete patient with the given id
-// server.del('/patients/:id', function (req, res, next) {
-//   console.log('DEL request: patient/' + req.params.id);
-//   Patients.remove({ _id: req.params.id }, function (error, result) {
-//     // If there are any errors, pass them to next in the correct format
-//     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+server.del('/patients/:id', function (req, res, next) {
+  console.log('DEL request: patient/' + req.params.id);
+  Patients.remove({ _id: req.params.id }, function (error, result) {
+    // If there are any errors, pass them to next in the correct format
+    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
 
-//     // Send a 200 OK response
-//     res.send()
-//   });
-// })
-
-
-
-// Find patient record by Id
-// server.get('/patients/:id/records', function (req, res, next){
-//   console.log('GET request: records');
-  
-//   Patients.findById({ _id: req.params.id }, (err, patient) => {
-//     if (err) {
-//         console.log(err);
-//         res.json(err);
-//     }
-//     if (!patient) {
-//         console.log(`->No patient with ID [${req.params.id}] found`);
-//        // res.status(404).json({ success: false, msg: "No Patient Found" })
-//     }
-//     else {
-//       Records.find({patient_id: req.params.id}).exec(function (error, record) {
-//         // If there are any errors, pass them to next in the correct format
-//         if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-    
-//         if (record) {
-//           // Send the record if no issues
-//           res.send(record)
-//         } else {
-//           // Send 404 header if the record doesn't exist
-//           res.send(404)
-//         }
-//       })
-
-//     }
-// })
-// })
-
-
-// // Add patient record
-// server.post('/patients/:id/records', function (req, res, next) {
-
-//   var newRecord = {
-//     patient_id: req.params.patient_id, 
-//     blood_pressure: req.params.blood_pressure,
-//     resp_rate: req.params.resp_rate,
-//     blood_oxygen: req.params.blood_oxygen,
-//     record_date: req.params.record_date
-//   };
-
-
-
-//       Patients.findById({ _id: req.params.id }, (err, patient) => {
-//           if (err) {
-//               console.log(err);
-//               res.json(err);
-//           }
-//           if (!patient) {
-//               console.log(`->No Patient with ID [${req.params.id}] found`);
-//               res.status(404).json({ success: false, msg: "No Patient Found" })
-//           }
-//           else {
-//               // Create the patient and saving to db
-//  newRecord.save(function (error, result) {
-
-//   // If there are any errors, pass them to next in the correct format
-//   if (error) console.log (error)
-
-//   // Send the patient if no issues
-//   res.send(201, result)
-// })
-// }
-// })
-// })
-
-
-
-// // delete Patient record
-// server.del('/patients/:id/record', function (req, res, next) {
-//   console.log('DEL request: records/' + req.params.id);
-//   Records.remove({ _id: req.params.id }, function (error, result) {
-//     // If there are any errors, pass them to next in the correct format
-//     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-//     // Send a 200 OK response
-//     res.send()
-//   });
-// })
+    // Send a 200 OK response
+    res.send()
+  });
+})
